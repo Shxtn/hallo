6dbc430bd8c5f34900134f38f250cca434562405
Shxtn <149594986+Shxtn@users.noreply.github.com>
2024-10-24 15:14:45 +0200

    Test Commit 2

--- src/main/java/edu/fra/uas/hallo/controller/HelloWorldController.java
+++ src/main/java/edu/fra/uas/hallo/controller/HelloWorldController.java
@@ -9,15 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
-
     
-
-
-
     @RequestMapping(value="hello", method=RequestMethod.GET)
     @ResponseBody
     public String getHelloWorld() {
         return "<h1>Hello World Spring Boot!<h1>";
+    }
+    public String newString(){
+        return "<h1> Bruh<h1>";
     }
     
 }
